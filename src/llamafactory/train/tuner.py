79df6be6bfbbdcb0b80633ddcad3132834aaff84
Manxi Sun <38352001+MaxineSun@users.noreply.github.com>
2024-09-28 15:54:21 +0800

    p

--- src/llamafactory/train/tuner.py
+++ src/llamafactory/train/tuner.py
@@ -44,6 +44,9 @@ def run_exp(args: Optional[Dict[str, Any]] = None, callbacks: List["TrainerCallb
     callbacks.append(LogCallback())
     model_args, data_args, training_args, finetuning_args, generating_args = get_train_args(args)
 
+    print(model_args, data_args, training_args, finetuning_args, generating_args)
+    print(p)
+
     if finetuning_args.stage == "pt":
         run_pt(model_args, data_args, training_args, finetuning_args, callbacks)
     elif finetuning_args.stage == "sft":
