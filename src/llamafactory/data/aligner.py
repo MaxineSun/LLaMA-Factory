dbf9d56923519643dcf7a9be1c50ea4891e82436
Manxi Sun <38352001+MaxineSun@users.noreply.github.com>
2024-10-14 15:39:14 +0800

    p

--- src/llamafactory/data/aligner.py
+++ src/llamafactory/data/aligner.py
@@ -87,9 +87,6 @@ def convert_alpaca(
             prompt.append({"role": Role.ASSISTANT.value, "content": old_response})
 
     query = []
-    print(dataset_attr.history)
-    print(example)
-    print(p)
     if dataset_attr.prompt and example[dataset_attr.prompt]:
         query.append(example[dataset_attr.prompt])
 
